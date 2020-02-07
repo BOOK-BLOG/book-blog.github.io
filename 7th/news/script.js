cbbfa63348ace650ac6415da55211f870c2332d3
SDEVXD <sdevxd@163.com>
2020-02-07 17:58:20 +0800

    Update

--- 7th/news/script.js
+++ 7th/news/script.js
@@ -1,107 +1,135 @@
-var consoleLogger = function (state, text) {
-  switch (state) {
-    case "success":
-      console.info("%c " + text + " ", "color:#00b700;background-color:#d9ffd9;border-radius:4px;padding:initial 4px;");
-      break;
-    default:
-      console.info(text);
-  }
+var consoleLogger = function(state, text) {
+    switch (state) {
+        case "success":
+            console.info("%c " + text + " ", "color:#00b700;background-color:#d9ffd9;border-radius:4px;padding:initial 4px;");
+            break;
+        default:
+            console.info(text);
+    }
 }
 
-var chartData;
+// Send the requests: 
+// https://lab.isaaclin.cn/nCoV/api/overall?latest=0
 var blankerlCrawler = new XMLHttpRequest();
 blankerlCrawler.open('GET', 'https://lab.isaaclin.cn/nCoV/api/overall?latest=0', true);
 blankerlCrawler.send();
-blankerlCrawler.onreadystatechange = function () {
-  if (blankerlCrawler.readyState == 4 && blankerlCrawler.status == 200) {
-    consoleLogger("success", ("Got response from: " + blankerlCrawler.responseURL));
-    var blankerlCrawlerResponse = blankerlCrawler.responseText;
-    blankerlCrawler.result = JSON.parse(blankerlCrawlerResponse);
-    // Set Data for Echarts: 
-    var blankerlCrawlerLength = blankerlCrawler.result.results["length"];
-    var conditionForWhile = blankerlCrawler.result.results["length"];
-    chartData = {
-      rawDate: new Array(),
-      date: new Array(),
-      confirmed: new Array(),
-      serious: new Array(),
-      suspected: new Array(),
-      dead: new Array(),
-      cured: new Array()
-    };
-    while (conditionForWhile > 0) {
-      chartData.rawDate[blankerlCrawlerLength - conditionForWhile] = new Date();
-      chartData.rawDate[blankerlCrawlerLength - conditionForWhile].setTime(blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].updateTime);
-      chartData.date[blankerlCrawlerLength - conditionForWhile] = chartData.rawDate[blankerlCrawlerLength - conditionForWhile].getDate();
-      chartData.confirmed[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].confirmedCount;
-      chartData.serious[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].seriousCount;
-      chartData.suspected[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].suspectedCount;
-      chartData.dead[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].deadCount;
-      chartData.cured[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].curedCount;
-      conditionForWhile--;
-    };
-    chartData.rawDate.reverse();
-    chartData.date.reverse();
-    chartData.confirmed.reverse();
-    chartData.serious.reverse();
-    chartData.suspected.reverse();
-    chartData.dead.reverse();
-    chartData.cured.reverse();
-    // Create Chart: 
-    var chart = echarts.init($("#chart")[0]);
-    chartSettings = {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross',
-          label: {
-            backgroundColor: '#808080'
-          }
+// https://lab.isaaclin.cn/nCoV/api/news?num=all
+var blankerlCrawlerNews = new XMLHttpRequest();
+blankerlCrawlerNews.open('GET', 'https://lab.isaaclin.cn/nCoV/api/news?num=10', true);
+blankerlCrawlerNews.send();
+
+var chartData;
+
+blankerlCrawler.onreadystatechange = function() {
+    if (blankerlCrawler.readyState == 4 && blankerlCrawler.status == 200) {
+        consoleLogger("success", ("Got response from: " + blankerlCrawler.responseURL));
+        var blankerlCrawlerResponse = blankerlCrawler.responseText;
+        blankerlCrawler.result = JSON.parse(blankerlCrawlerResponse);
+        // Set Data for Echarts: 
+        var blankerlCrawlerLength = blankerlCrawler.result.results["length"];
+        var conditionForWhile = blankerlCrawler.result.results["length"];
+        chartData = {
+            rawDate: new Array(),
+            date: new Array(),
+            confirmed: new Array(),
+            serious: new Array(),
+            suspected: new Array(),
+            dead: new Array(),
+            cured: new Array()
+        };
+        while (conditionForWhile > 0) {
+            chartData.rawDate[blankerlCrawlerLength - conditionForWhile] = new Date();
+            chartData.rawDate[blankerlCrawlerLength - conditionForWhile].setTime(blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].updateTime);
+            chartData.date[blankerlCrawlerLength - conditionForWhile] = chartData.rawDate[blankerlCrawlerLength - conditionForWhile].getDate();
+            chartData.confirmed[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].confirmedCount;
+            chartData.serious[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].seriousCount;
+            chartData.suspected[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].suspectedCount;
+            chartData.dead[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].deadCount;
+            chartData.cured[blankerlCrawlerLength - conditionForWhile] = blankerlCrawler.result.results[blankerlCrawlerLength - conditionForWhile].curedCount;
+            conditionForWhile--;
+        };
+        chartData.rawDate.reverse();
+        chartData.date.reverse();
+        chartData.confirmed.reverse();
+        chartData.serious.reverse();
+        chartData.suspected.reverse();
+        chartData.dead.reverse();
+        chartData.cured.reverse();
+        // Create Chart: 
+        var chart = echarts.init($("#chart")[0]);
+        chartSettings = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross',
+                    label: {
+                        backgroundColor: '#808080'
+                    }
+                }
+            },
+            legend: {
+                data: ['确诊', '重症', '疑似', '死亡', '治愈'],
+            },
+            xAxis: {
+                type: 'category',
+                data: chartData.date
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                    name: '确诊',
+                    data: chartData.confirmed,
+                    type: 'line',
+                    smooth: true
+                },
+                {
+                    name: '重症',
+                    data: chartData.suspected,
+                    type: 'line',
+                    smooth: true
+                },
+                {
+                    name: '疑似',
+                    data: chartData.suspected,
+                    type: 'line',
+                    smooth: true
+                },
+                {
+                    name: '死亡',
+                    data: chartData.dead,
+                    type: 'line',
+                    smooth: true
+                },
+                {
+                    name: '治愈',
+                    data: chartData.cured,
+                    type: 'line',
+                    smooth: true
+                }
+            ]
+        };
+        chart.setOption(chartSettings);
+        consoleLogger("success", ("Chart is created successfully"));
+    }
+};
+
+blankerlCrawlerNews.onreadystatechange = function() {
+    if (blankerlCrawlerNews.readyState == 4 && blankerlCrawlerNews.status == 200) {
+        consoleLogger("success", ("Got response from: " + blankerlCrawlerNews.responseURL));
+        var blankerlCrawlerNewsResponse = blankerlCrawlerNews.responseText;
+        blankerlCrawlerNews.result = JSON.parse(blankerlCrawlerNewsResponse);
+        $("#loading").remove();
+        var blankerlCrawlerNewsLength = blankerlCrawlerNews.result.results["length"];
+        var conditionForWhile = blankerlCrawlerNews.result.results["length"];
+        while (conditionForWhile > 0) {
+            var resultsCache = blankerlCrawlerNews.result.results[blankerlCrawlerNewsLength - conditionForWhile];
+            var StrEle = "<div><div class=\"news-content\"><h1>" + resultsCache.title + "</h1><p>" + resultsCache.summary + "</p><a class\"primary-button\" href=\"" + resultsCache.sourceUrl + "\">前往 " + resultsCache.infoSource + " 以查看更多</a></div></div>";
+            var Ele = $(StrEle).addClass("news");
+            $("#news-container").append(Ele);
+            conditionForWhile--;
         }
-      },
-      legend: {
-        data: ['确诊', '重症', '疑似', '死亡', '治愈'],
-      },
-      xAxis: {
-        type: 'category',
-        data: chartData.date
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          name: '确诊',
-          data: chartData.confirmed,
-          type: 'line',
-          smooth: true
-        },
-        {
-          name: '重症',
-          data: chartData.suspected,
-          type: 'line',
-          smooth: true
-        },
-        {
-          name: '疑似',
-          data: chartData.suspected,
-          type: 'line',
-          smooth: true
-        },
-        {
-          name: '死亡',
-          data: chartData.dead,
-          type: 'line',
-          smooth: true
-        },
-        {
-          name: '治愈',
-          data: chartData.cured,
-          type: 'line',
-          smooth: true
-        }
-      ]
-    };
-    chart.setOption(chartSettings);
-  }
+        $("#news-container").append($("<a href=\"http://www.myzaker.com/channel/22068\">前往 ZAKER 频道：直击新冠肺炎疫情</a>").addClass("primary-button"));
+        consoleLogger("success", ("News is shown successfully"));
+    }
 };
--- 7th/script.js
+++ 7th/script.js
@@ -1,136 +1,138 @@
 var currentTime = new Date()
 
 var consoleLogger = function(state, text) {
-  switch (state) {
-    case "success":
-      console.info("%c " + text + " ", "color:#00b700;background-color:#d9ffd9;border-radius:4px;padding:initial 4px;");
-      break;
-    default:
+    switch (state) {
+        case "success":
+            console.info("%c " + text + " ", "color:#00b700;background-color:#d9ffd9;border-radius:4px;padding:initial 4px;");
+            break;
+        default:
 
-  }
+    }
 }
 
 var blankerlCrawler = new XMLHttpRequest();
 blankerlCrawler.open('GET', 'https://lab.isaaclin.cn/nCoV/api/overall', true);
 blankerlCrawler.send();
 blankerlCrawler.onreadystatechange = function() {
-  if (blankerlCrawler.readyState == 4 && blankerlCrawler.status == 200) {
-    consoleLogger("success", ("Got response from: " + blankerlCrawler.responseURL));
-    var blankerlCrawlerResponse = blankerlCrawler.responseText;
-    blankerlCrawler.result = JSON.parse(blankerlCrawlerResponse);
-    $("#current-time-for-current-data").html(String(currentTime.getFullYear()) + " 年 " + String(currentTime.getMonth() + 1) + " 月 " + currentTime.getDate() + " 日");
-    $("#confirmedCount").html(blankerlCrawler.result.results[0].confirmedCount);
-    $("#suspectedCount").html(blankerlCrawler.result.results[0].suspectedCount);
-    $("#deadCount").html(blankerlCrawler.result.results[0].deadCount);
-    $("#curedCount").html(blankerlCrawler.result.results[0].curedCount);
-    $("#confirmedIncr").html("+" + blankerlCrawler.result.results[0].confirmedIncr);
-    $("#suspectedIncr").html("+" + blankerlCrawler.result.results[0].suspectedIncr);
-    $("#deadIncr").html("+" + blankerlCrawler.result.results[0].deadIncr);
-    $("#curedIncr").html("+" + blankerlCrawler.result.results[0].curedIncr);
-  }
+    if (blankerlCrawler.readyState == 4 && blankerlCrawler.status == 200) {
+        consoleLogger("success", ("Got response from: " + blankerlCrawler.responseURL));
+        var blankerlCrawlerResponse = blankerlCrawler.responseText;
+        blankerlCrawler.result = JSON.parse(blankerlCrawlerResponse);
+        $("#current-time-for-current-data").html(String(currentTime.getFullYear()) + " 年 " + String(currentTime.getMonth() + 1) + " 月 " + currentTime.getDate() + " 日");
+        $("#confirmedCount").html(blankerlCrawler.result.results[0].confirmedCount);
+        $("#suspectedCount").html(blankerlCrawler.result.results[0].suspectedCount);
+        $("#deadCount").html(blankerlCrawler.result.results[0].deadCount);
+        $("#curedCount").html(blankerlCrawler.result.results[0].curedCount);
+        $("#confirmedIncr").html("+" + blankerlCrawler.result.results[0].confirmedIncr);
+        $("#suspectedIncr").html("+" + blankerlCrawler.result.results[0].suspectedIncr);
+        $("#deadIncr").html("+" + blankerlCrawler.result.results[0].deadIncr);
+        $("#curedIncr").html("+" + blankerlCrawler.result.results[0].curedIncr);
+    }
 };
 
 function createMarker(icon, x, y, title, address, source, name, telephone, type) {
-  var marker = new AMap.Marker({
-    position: new AMap.LngLat(x, y),
-    icon: icon,
-    title: title,
-    anchor: 'bottom-center',
-    bookBlogInfo: {
-      x: x,
-      y: y,
-      title: title,
-      address: address,
-      source: source,
-      name: name,
-      telephone: telephone,
-      type: type
-    }
-  })
-  marker.on('click', function() {
-    map.setZoom(17);
-    map.setFitView(marker);
-    $(".detail").addClass("hide");
-    $(".controls").addClass("hide");
-    $("[info=\"title\"]").html(title);
-    $("[info=\"address\"]").html(address);
-    $("[info=\"source\"]").html(source);
-    $("[info=\"source\"]").attr("href", source);
-    $("[info=\"name\"]").html(name);
-    $("[info=\"telephone\"]").html(telephone);
-    $("[info=\"telephone\"]").attr("href", ("tel:" + telephone));
-    $("button[open-in=\"amap\"]").click(function() {
-      marker.markOnAMAP({
-        name: title,
-        position: marker.getPosition()
-      })
-    });
-    $("button[open-in=\"apple-map\"]").click(function() {
-      window.location.href = "https://maps.apple.com/?q=" + address;
-    });
-    setTimeout(function() {
-      $(".detail").removeClass("hide");
-    }, 350)
-  })
-  return marker;
+    var marker = new AMap.Marker({
+        position: new AMap.LngLat(x, y),
+        icon: icon,
+        title: title,
+        anchor: 'bottom-center',
+        bookBlogInfo: {
+            x: x,
+            y: y,
+            title: title,
+            address: address,
+            source: source,
+            name: name,
+            telephone: telephone,
+            type: type
+        }
+    })
+    marker.on('click', function() {
+        map.setZoom(17);
+        map.setFitView(marker);
+        $(".detail").addClass("hide");
+        $(".controls").addClass("hide");
+        $("[info=\"title\"]").html(title);
+        $("[info=\"address\"]").html(address);
+        $("[info=\"source\"]").html(source);
+        $("[info=\"source\"]").attr("href", source);
+        $("[info=\"name\"]").html(name);
+        $("[info=\"telephone\"]").html(telephone);
+        $("[info=\"telephone\"]").attr("href", ("tel:" + telephone));
+        $("button[open-in=\"amap\"]").click(function() {
+            marker.markOnAMAP({
+                name: title,
+                position: marker.getPosition()
+            })
+        });
+        $("button[open-in=\"apple-map\"]").click(function() {
+            window.location.href = "https://maps.apple.com/?q=" + address;
+        });
+        setTimeout(function() {
+            $(".detail").removeClass("hide");
+        }, 350)
+    })
+    return marker;
 };
 
 var marker = {
-  hospital: {
-    marker: [
-      createMarker("./icon/hospital-marker.png", 114.275458, 30.584738, "协和医院", "武汉市江汉区汉口解放大道1277号", "https://news.sina.com.cn/gn/2020-01-30/detail-iimxxste7673100.d.html", "胡秋实", "13707162353", "hospital")
-    ]
-  },
-  protect: []
+    hospital: {
+        marker: [
+            createMarker("./icon/hospital-marker.png", 114.275458, 30.584738, "协和医院", "武汉市江汉区汉口解放大道1277号", "https://news.sina.com.cn/gn/2020-01-30/detail-iimxxste7673100.d.html", "胡秋实", "13707162353", "hospital")
+        ]
+    },
+    protect: []
 };
 
 $(".list-down").click(function() {
-  $(".controls").addClass("down");
-  $(".list-down").addClass("hide");
-  $(".list-up").removeClass("hide");
+    $(".controls").addClass("down");
+    $(".list-down").addClass("hide");
+    $(".list-up").removeClass("hide");
 })
 $(".list-up").click(function() {
-  $(".controls").removeClass("down");
-  $(".list-up").addClass("hide");
-  $(".list-down").removeClass("hide");
+    $(".controls").removeClass("down");
+    $(".list-up").addClass("hide");
+    $(".list-down").removeClass("hide");
 })
 
 $("#hospital-length").html(marker.hospital.marker["length"]);
 
+$(".list-item[type=\"current-data\"]").click(function() { window.location.href = "./news/" })
+
 $(".refresh").click(function() {
-  location.reload();
+    location.reload();
 })
 
 $(".change-source").click(function() {
-  var hrefer;
-  if (window.location.href.search("sdevxd.github.io") != -1) {
-    hrefer = "https://book-blog.github.io/7th/";
-  } else if (window.location.href.search("book-blog.github.io") != -1) {
-    hrefer = "http://kt7yvd.coding-pages.com/7th/";
-  } else if (window.location.href.search("kt7yvd.coding-pages.com") != -1) {
-    hrefer = "https://book-blog.github.io/7th/";
-  }
-  window.location.href = hrefer;
+    var hrefer;
+    if (window.location.href.search("sdevxd.github.io") != -1) {
+        hrefer = "https://book-blog.github.io/7th/";
+    } else if (window.location.href.search("book-blog.github.io") != -1) {
+        hrefer = "http://kt7yvd.coding-pages.com/7th/";
+    } else if (window.location.href.search("kt7yvd.coding-pages.com") != -1) {
+        hrefer = "https://book-blog.github.io/7th/";
+    }
+    window.location.href = hrefer;
 })
 
 // amap
 var map = new AMap.Map('map');
 map.on('complete', function() {
-  consoleLogger("success", "Map is created successfully");
-  $(".controls").removeClass("hide");
-  map.add(marker.hospital.marker);
-  $(".list-item[type=\"hospital\"]").click(function() {
-    $(".controls").addClass("down");
-    $(".list-down").addClass("hide");
-    $(".list-up").removeClass("hide");
-    map.setFitView(marker.hospital.marker);
-    $("[src=\"./icon/" + $(this).attr("type") + "-marker.png\"]").addClass("active");
-    $(this).addClass("active");
-  })
+    consoleLogger("success", "Map is created successfully");
+    $(".controls").removeClass("hide");
+    map.add(marker.hospital.marker);
+    $(".list-item[type=\"hospital\"]").click(function() {
+        $(".controls").addClass("down");
+        $(".list-down").addClass("hide");
+        $(".list-up").removeClass("hide");
+        map.setFitView(marker.hospital.marker);
+        $("[src=\"./icon/" + $(this).attr("type") + "-marker.png\"]").addClass("active");
+        $(this).addClass("active");
+    })
 })
 map.on('click', function() {
-  $(".amap-icon img.active").removeClass("active");
-  $(".list-item.active[type]").removeClass("active");
-  $(".detail").addClass("hide");
-  $(".controls").removeClass("hide");
+    $(".amap-icon img.active").removeClass("active");
+    $(".list-item.active[type]").removeClass("active");
+    $(".detail").addClass("hide");
+    $(".controls").removeClass("hide");
 })
